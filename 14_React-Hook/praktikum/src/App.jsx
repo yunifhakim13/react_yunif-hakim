34e1307936c96baa54736bdac2e4f1dec332972d
YunifHakim <yunifhakim@gmail.com>
2023-09-18 01:59:37 +0700

    aaa

--- 14_React-Hook/praktikum/src/App.jsx
+++ 14_React-Hook/praktikum/src/App.jsx
@@ -1,190 +1,282 @@
-import Navigation from "./Components/Navigation";
-import "./App.css";
-import InputWithLabel from "./Components/InputWithLabel";
-import InputRadio from "./Components/InputRadio";
-import BsLogo from "./assets/bootstrap-logo.svg.png";
-import { article } from "./article";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import logobs from "./assets/bootstrap-logo.svg.png";
+import "./index.css";
+function App() {
+  useEffect(() => {
+    alert("Welcome");
+  }, []);
 
-export default function App() {
-  const handleClick = () => {
-    const randomNumber = Math.floor(Math.random() * 100);
-    console.log("Random Number:", randomNumber);
-  };
-
-  const [productName, setProductName] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const handleProductNameChange = (e) => {
-    const value = e.target.value;
-
-    if (value.length > 25) {
-      setErrorMessage("Product Name must not exceed 25 characters.");
-    } else if (value.length > 10) {
-      setErrorMessage("Product Name must not exceed 10 characters.");
-    } else if (value.trim() === "") {
-      setErrorMessage("Please enter a valid product name.");
-    } else {
-      setErrorMessage("");
-    }
-
-    setProductName(value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault(); // Untuk menghindari pembaruan halaman
-
-    if (errorMessage === "") {
-      console.log("Product Name:", productName);
-      setProductName("");
-    } else {
-      alert("Please fix the errors before submitting.");
-    }
-  };
   return (
     <>
-      <Navigation />
-      <section className="section">
+      <nav className="navbar navbar-expand-lg" id="customNav">
         <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-2"></div>
-            <div className="col-md-8">
-              <img
-                className="img-fluid mx-auto d-block"
-                style={{
-                  display: "inline - block",
-                  marginTop: "20px",
-                  width: "80px",
-                }}
-                src={BsLogo}
-                alt="img-bootstrap"
-              />
-              <div className="mt-3">
-                <h1 className="text-center">{article.title.en}</h1>
-                <p className="text-center">{article.description.en}</p>
-                <br />
-                <br />
-                <div className="row">
-                  <div className="col-md-2"></div>
-                  <div className="col-md-8">
-                    <h3>Detail Product</h3>
-                    <form id="productForm" onSubmit={handleSubmit}>
-                      <InputWithLabel
-                        labelClass={"form-label"}
-                        labelFor={"productname"}
-                        labelText={"Product name"}
-                        inputId={"productName"}
-                        inputClassName={"form-control"}
-                        inputType={"text"}
-                        ariaLabel={"default input example"}
-                        onChange={handleProductNameChange}
-                      />
-                      {errorMessage && (
-                        <p style={{ color: "red" }}>{errorMessage}</p>
-                      )}
-
-                      <div className="mb-3 col-md-5">
-                        <label for="productcategory" className="form-label">
-                          Product Category
-                        </label>
-                        <select id="productSelect" class="form-select">
-                          <option value="">Choose</option>
-                          <option value="Kasur">Kasur</option>
-                          <option value="Bantal">Bantal</option>
-                        </select>
-                      </div>
-                      <div className="mb-3  col-md-5">
-                        <label for="image" class="form-label pimary">
-                          Image of Product
-                        </label>
-                        <input
-                          className="form-control"
-                          type="file"
-                          id="formFile"
-                        />
-                      </div>
-                      <div className="mb-3">
-                        <label for="freshness" class="form-label">
-                          Product Freshness
-                        </label>
-                        <InputRadio
-                          radioFor={"flexRadioDefault1"}
-                          radioValue={"Brand New"}
-                          radioText={"Brand New"}
-                        />
-                        <InputRadio
-                          radioFor={"flexRadioDefault2"}
-                          radioValue={"Second Hand"}
-                          radioText={"Second Hand"}
-                        />
-                        <InputRadio
-                          radioFor={"flexRadioDefault3"}
-                          radioValue={"Refurbished"}
-                          radioText={"Refurbished"}
-                        />
-                      </div>
-                      <div class="mb-2">
-                        <label
-                          for="exampleFormControlTextarea1"
-                          className="form-label">
-                          Additional Description
-                        </label>
-                      </div>
-                      <div class="form-floating mb-2">
-                        <textarea
-                          className="form-control"
-                          placeholder="Leave a comment here"
-                          id="textArea"
-                          style={{ height: "100px" }}></textarea>
-                      </div>
-                      <InputWithLabel
-                        labelFor={"exampleFormControlTextarea1"}
-                        labelClass={"form-label"}
-                        labelText={"Product Price"}
-                        inputId={"productPrice"}
-                        inputClassName={"form-control"}
-                        inputType={"number"}
-                        ariaLabel={"default input example"}
-                        placehholder={"$ 1"}
-                      />
-                      <button
-                        onClick={handleSubmit}
-                        type="submit"
-                        className="btn btn-primary"
-                        style={{ marginTop: "100px" }}>
-                        Submit
-                      </button>
-                    </form>
-                    <button
-                      onClick={handleClick}
-                      type="submit"
-                      className="btn btn-primary"
-                      style={{ marginTop: "100px" }}>
-                      generate random number
-                    </button>
-                  </div>
-                  <div className="col-md-3"></div>
-                </div>
-              </div>
+          <a className="navbar-brand ms-2" href="#">
+            Simple Header
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav ms-auto">
+              <a className="btn btn-primary" href="#">
+                Home
+              </a>
+              <a className="nav-link" href="#">
+                Features
+              </a>
+              <a className="nav-link" href="#">
+                Pricing
+              </a>
+              <a className="nav-link" href="#">
+                FAQs
+              </a>
+              <a className="nav-link" href="#">
+                About
+              </a>
             </div>
           </div>
         </div>
+      </nav>
+      <section>
+        <div
+          className="d-flex justify-content-center"
+          style={{ marginTop: "58px" }}>
+          <img src={logobs} alt="logobs" />
+        </div>
+        <div className="d-flex flex-column justify-content-center align-items-center">
+          <h1>Create Account</h1>
+          <p className="text-center">
+            Below is an example form built entirely with Bootstrap’s form
+            controls. Each required form group has a validation state that can
+            be triggered by attempting to submit the form without completing it.
+          </p>
+        </div>
+        <div style={{ marginLeft: "392px", marginRight: "392px" }}>
+          <form id="myForm" className="row g-3 needs-validation" novalidate>
+            <div className="col-md-6">
+              <label for="validationCustom01" className="form-label">
+                First name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="validationCustom01"
+                required
+              />
+              <div className="valid-feedback">Looks good!</div>
+            </div>
+            <div className="col-md-6">
+              <label for="validationCustom02" className="form-label">
+                Last name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="validationCustom02"
+                required
+              />
+              <div className="valid-feedback">Looks good!</div>
+            </div>
+            <div className="col-md-12">
+              <label for="validationCustomUsername" className="form-label">
+                Username
+              </label>
+              <div className="input-group has-validation">
+                <span className="input-group-text" id="inputGroupPrepend">
+                  @
+                </span>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="validationCustomUsername"
+                  aria-describedby="inputGroupPrepend"
+                  required
+                />
+                <div className="invalid-feedback">
+                  Please choose a username.
+                </div>
+              </div>
+            </div>
+            <div className="col-md-12">
+              <label for="validationCustom03" className="form-label">
+                Email
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="validationCustom03"
+                placeholder="you@email.com"
+                required
+              />
+              <div className="invalid-feedback">
+                Please provide a valid city.
+              </div>
+            </div>
+            <div>
+              <label>Gender</label>
+              <div className="form-check">
+                <input
+                  type="radio"
+                  className="form-check-input"
+                  id="validationFormCheck2"
+                  name="radio-stacked"
+                  value="Male"
+                  required
+                />
+                <label className="form-check-label" for="validationFormCheck2">
+                  Male
+                </label>
+              </div>
+              <div className="form-check">
+                <input
+                  type="radio"
+                  className="form-check-input"
+                  id="validationFormCheck2"
+                  name="radio-stacked"
+                  required
+                  value="Famale"
+                />
+                <label className="form-check-label" for="validationFormCheck2">
+                  Famale
+                </label>
+              </div>
+              <div className="form-check mb-3">
+                <input
+                  type="radio"
+                  className="form-check-input"
+                  id="validationFormCheck3"
+                  name="radio-stacked"
+                  required
+                  value="Other"
+                />
+                <label className="form-check-label" for="validationFormCheck3">
+                  Other
+                </label>
+                <div className="invalid-feedback">
+                  More example invalid feedback text
+                </div>
+              </div>
+            </div>
+            <div className="col-md-12">
+              <label for="validationCustom03" className="form-label">
+                Address
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="validationCustom05"
+                placeholder="1234 Main St"
+                required
+              />
+              <div className="invalid-feedback">
+                Please provide a valid address.
+              </div>
+            </div>
+            <div className="col-md-12">
+              <label for="validationCustom03" className="form-label">
+                Address(optional)
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="validationCustom06"
+                placeholder="Apartment of suite"
+                required
+              />
+              <div className="invalid-feedback">
+                Please provide a valid address.
+              </div>
+            </div>
+            <div className="col-md-3">
+              <label for="validationCustom04" className="form-label">
+                Nationality
+              </label>
+              <select className="form-select" id="validationCustom04" required>
+                <option selected disabled value="">
+                  Choose...
+                </option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+              <div className="invalid-feedback">
+                Please select a valid nationality.
+              </div>
+            </div>
+            <div
+              style={{
+                width: "100%",
+                height: "1px",
+                backgroundColor: "black",
+              }}></div>
+            <div className="col-12">
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value="Bahasa"
+                  id="invalidCheck"
+                />
+                <label className="form-check-label" for="invalidCheck">
+                  Bahasa
+                </label>
+              </div>
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value="English"
+                  id="invalidCheck01"
+                />
+                <label className="form-check-label" for="invalidCheck">
+                  English
+                </label>
+              </div>
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value="Others"
+                  id="invalidCheck02"
+                />
+                <label className="form-check-label" for="invalidCheck">
+                  Others
+                </label>
+              </div>
+            </div>
+            <div className="col-12 d-flex justify-content-center">
+              <button className="btn btn-primary" id="submitBtn" type="submit">
+                Create Account
+              </button>
+            </div>
+          </form>
+        </div>
       </section>
-      <section style={{ marginBottom: "400px" }}>
-        <h2 className="text-center">List Product</h2>
+      <section className="py-3 px-4">
+        <h1 className="py-2 text-center">List Account</h1>
         <table className="table table-striped">
           <thead>
             <tr>
               <th scope="col">No</th>
-              <th scope="col">Product Name</th>
-              <th scope="col">Product Category </th>
-              <th scope="col">Image of Product</th>
-              <th scope="col">Product Freshness</th>
-              <th scope="col">Additional Desciption</th>
-              <th scope="col">Product Price</th>
+              <th scope="col">First Name</th>
+              <th scope="col">Last Neme</th>
+              <th scope="col">Username</th>
+              <th scope="col">Email</th>
+              <th scope="col">Gender</th>
+              <th scope="col">Address</th>
+              <th scope="col">Address 2</th>
+              <th scope="col">Nationality</th>
+              <th scope="col">Language</th>
             </tr>
           </thead>
-          <tbody id="productLists"></tbody>
+          <tbody id="userData"></tbody>
         </table>
 
         <input
@@ -199,10 +291,7 @@ export default function App() {
           className="btn-group"
           role="group"
           aria-label="Basic outlined example">
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick="hapusBaris(this)">
+          <button type="button" className="btn btn-primary">
             Delection
           </button>
           <button type="button" className="btn btn-outline-primary">
@@ -213,3 +302,5 @@ export default function App() {
     </>
   );
 }
+
+export default App;
