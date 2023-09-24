83dec0d1adddd226d4fb575fc65dfe92d0d1f5d4
YunifHakim <yunifhakim@gmail.com>
2023-09-25 00:55:44 +0700

    update submission

--- 15_React-Routing/praktikum/src/App.jsx
+++ 15_React-Routing/praktikum/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CreateProduct from "./Components/CreateProduct/CreateProduct.jsx";
 import LandingPage from "./Components/LandingPage/LandingPage.jsx";
+import DetailProduct from "./Components/CreateProduct/DetailProduct.jsx";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={CreateProduct} />
         <Route path="/LandingPage" component={LandingPage} />
+        <Route path="/Detail/:id" component={DetailProduct} />
       </Switch>
     </Router>
   );
